3b668f28851bd344b01f5ea54348a2a871b11165
TAMARA YAEL DRATLER <47807159@ort>
2023-11-02 08:57:27 -0300

    2/11

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,6 +5,13 @@ namespace ProyectoFinal4to.Controllers;
 public class HomeController : Controller
 {
 
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    /*REGISTRARSE*/
+
     public IActionResult C_Registrarse()
     {
         return View("Registrarse");
@@ -15,8 +22,40 @@ public class HomeController : Controller
         return View("RegistrarseMascota");
     }
 
-    public IActionResult Index()
+    public IActionResult C_CrearAntecedente()
     {
-        return View();
+        return View("CrearAntecedente");
+    }
+
+    public IActionResult C_CrearVacuna()
+    {
+        return View("CrearVacuna");
+    }
+    
+    public IActionResult C_CrearNota()
+    {
+        return View("CrearNota");
+    }
+
+    /*INICIAR SESION*/
+    
+    public IActionResult C_IniciarSesion()
+    {
+        return View("IniciarSesion");
+    }
+    
+    public IActionResult C_OlvideContrasena()
+    {
+        return View("OlvideContrasena");
+    }
+    
+    public IActionResult C_ElegirMascota()
+    {
+        return View("ElegirMascota");
+    }
+    
+    public IActionResult C_Home()
+    {
+        return View("Home");
     }
 }

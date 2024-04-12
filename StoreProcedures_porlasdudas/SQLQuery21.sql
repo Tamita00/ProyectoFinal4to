90c1c1d759401f87b4a361c818ecb78326c61530
TAMARA YAEL DRATLER <47807159@ort>
2024-04-12 11:56:40 -0300

    12/4

---
+++ StoreProcedures_porlasdudas/SQLQuery21.sql
@@ -0,0 +1,15 @@
+USE [MiMascota]
+GO
+/****** Object:  StoredProcedure [dbo].[sp_CrearDueno]    Script Date: 12/4/2024 11:43:17 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[sp_MostrarRaza]
+@RazaReci varchar(8000)
+AS
+BEGIN
+	SELECT * FROM Raza WHERE Raza.Raza = @RazaReci;
+	
+END
